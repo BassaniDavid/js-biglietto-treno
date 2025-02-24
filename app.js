16594a9cd9f98bf6187af42dd59c8f2761da05f4
BassaniDavid <davbassan98@gmail.com>
2025-02-24 16:26:21 +0100

    feat: final version3

--- app.js
+++ app.js
@@ -23,12 +23,15 @@ if (isNaN(age)){
 if (age < 18 ){
     let totalCost = (totalKm * costKm) / 100 * 80
     console.log(nameSurname + " il costo del tuo biglietto è " + totalCost.toFixed(2) + " €")
+    alert(nameSurname + " il costo del tuo biglietto è " + totalCost.toFixed(2) + " €")
 } 
 else if ( age > 65 ){
     let totalCost = (totalKm * costKm) / 100 * 60
     console.log(nameSurname + " il costo del tuo biglietto è " + totalCost.toFixed(2) + " €")
+    alert(nameSurname + " il costo del tuo biglietto è " + totalCost.toFixed(2) + " €")
 }
 else{
     let totalCost = (totalKm * costKm)
     console.log(nameSurname + " il costo del tuo biglietto è " + totalCost.toFixed(2) + " €")
+    alert(nameSurname + " il costo del tuo biglietto è " + totalCost.toFixed(2) + " €")
 }
