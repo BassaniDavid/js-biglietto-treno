b6d15222b94a1c41dd43380bc2a8bc439432355c
BassaniDavid <davbassan98@gmail.com>
2025-02-24 16:12:46 +0100

    feat: second version

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 progetto: Biglietto del treno
 
-procedimento
+procedimento:
 
 1 chiedo nome e cognome e li assegno ad una variabile
 2 chiedo il numero di km e li assegno
 3 chiedo l'età e la assegno
 4 calcolo il costo:
     se l'età è minore di 18 anni applico il 20%
-    se è compresa tra 18 e 65 il costo rimane invariato
     se è maggiore di 65 applico il 40%
+    se è compresa tra 18 e 65 il costo rimane invariato
+
--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-const nameSurname = "nome cognome "
+console.log("il codice darà NaN finchè non si inseriranno i campi")
 
-const totalKm = "100"
+const nameSurname = "nome cognome"
+
+const totalKm = "numero km"
 
 const costKm = 0.21
 
-const age = "165"
+const age = "età"
 
 
 if (age < 18 ){
     let totalCost = (totalKm * costKm) / 100 * 80
-    console.log(nameSurname + " il costo del tuo biglietto è " + totalCost + " €")
+    console.log(nameSurname + " il costo del tuo biglietto è " + totalCost.toFixed(2) + " €")
 } 
 else if ( age > 65 ){
     let totalCost = (totalKm * costKm) / 100 * 60
-    console.log(nameSurname + " il costo del tuo biglietto è " + totalCost + " €")
+    console.log(nameSurname + " il costo del tuo biglietto è " + totalCost.toFixed(2) + " €")
 }
 else{
     let totalCost = (totalKm * costKm)
-    console.log(nameSurname + " il costo del tuo biglietto è " + totalCost + " €")
+    console.log(nameSurname + " il costo del tuo biglietto è " + totalCost.toFixed(2) + " €")
 }
