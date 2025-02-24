999f0880f71c1271a5c11a23af32e00453fbd88a
BassaniDavid <davbassan98@gmail.com>
2025-02-24 19:01:04 +0100

    correct version2

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 progetto: Biglietto del treno
 
 procedimento:
+-chiedo nome e cognome e lo assegno ad una variabile 
+-chiedo il numero di km e li assegno
+-chiedo l'età e la assegno
+-controllo se i km e l età è effettivamente un numero
 
-1 chiedo il numero di km e li assegno
-2 chiedo l'età e la assegno
-3 controllo se i km e l età è effettivamente un numero
-
-4 calcolo il costo:
+-calcolo il costo:
     se l'età è minore di 18 anni applico il 20%
     se è maggiore di 65 applico il 40%
     se è compresa tra 18 e 65 il costo rimane invariato
--- app.js
+++ app.js
@@ -1,30 +1,34 @@
+let nameSurname = prompt("per favore, inserisci il nome e cognome");
 
+if (nameSurname == null){
+    alert("per favore, inserire un nome e cognome")
+};
 
 let totalKm = prompt("per favore, inserisci il numero di km");
 
-const costKm = 0.21
+const costKm = 0.21;
 
 let age = prompt("per favore, inserisci l'età");
 
 if (isNaN(totalKm)){
     alert("per favore, inserisci un numero di km valido")
-}
+};
 if (isNaN(age)){
     alert("per favore, inserisci un età valida")
-}
+};
 
 if (age < 18 ){
     let totalCost = (totalKm * costKm) / 100 * 80
-    console.log( " il costo del tuo biglietto è " + totalCost.toFixed(2) + " €")
-    alert( " il costo del tuo biglietto è " + totalCost.toFixed(2) + " €")
-} 
+    console.log( nameSurname + " il costo del tuo biglietto è " + totalCost.toFixed(2) + " €")
+    alert( nameSurname +  " il costo del tuo biglietto è " + totalCost.toFixed(2) + " €")
+}
 else if ( age > 65 ){
     let totalCost = (totalKm * costKm) / 100 * 60
-    console.log( " il costo del tuo biglietto è " + totalCost.toFixed(2) + " €")
-    alert( " il costo del tuo biglietto è " + totalCost.toFixed(2) + " €")
+    console.log( nameSurname + " il costo del tuo biglietto è " + totalCost.toFixed(2) + " €")
+    alert( nameSurname + " il costo del tuo biglietto è " + totalCost.toFixed(2) + " €")
 }
 else{
     let totalCost = (totalKm * costKm)
-    console.log( " il costo del tuo biglietto è " + totalCost.toFixed(2) + " €")
-    alert( " il costo del tuo biglietto è " + totalCost.toFixed(2) + " €")
+    console.log( nameSurname + " il costo del tuo biglietto è " + totalCost.toFixed(2) + " €")
+    alert( nameSurname + " il costo del tuo biglietto è " + totalCost.toFixed(2) + " €")
 }
